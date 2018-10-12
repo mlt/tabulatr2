e2724bc7e350835a12a058dbe6176b4e2a50661a
Mikhail Titov <mlt@gmx.us>
2018-10-12 13:43:15 -0500

    Capybara screenshot gem added

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,12 +9,28 @@ ActiveRecord::Migrator.migrate File.expand_path("../dummy/db/migrate/", __FILE__
 
 require 'rspec/rails'
 require 'capybara/rspec'
+require 'capybara-screenshot/rspec'
 require 'spec_helper'
 
-Capybara.javascript_driver = :selenium_chrome_headless
+Capybara.javascript_driver = :chrome # :selenium_chrome_headless
 Capybara.server = :webrick
 
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.load_selenium
+  browser_options = ::Selenium::WebDriver::Chrome::Options.new
+  browser_options.args << '--headless'
+  browser_options.args << '--window-size=1366,768'
+  browser_options.args << '--disable-gpu' if Gem.win_platform?
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+end
 
+Capybara::Screenshot.register_driver(:chrome) do |driver, path|
+  driver.browser.save_screenshot(path)
+end
+
+Capybara::Screenshot.register_filename_prefix_formatter(:rspec) do |example|
+  "screenshot_#{example.description.gsub(' ', '-').gsub(/^.*\/spec\//,'')}"
+end
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
